95cb105685d0a0ce460b39768d822e2905095a87
Andreas Nilsson <anil9@kth.se>
2015-03-10 15:11:08 +0100

    Created layer structure

---
+++ Layer.java
@@ -0,0 +1,32 @@
+public class Layer {
+
+	public Corner cubie1Corner; public Edge cubie2Edge; public Corner cubie3Corner;
+	public Edge cubie4Corner; public Center cubie5Center; public Edge cubie6Edge;
+	public Corner cubie7Corner; public Edge cubie8Edge; public Corner cubie9Corner;
+
+
+	/*
+		structure is:
+		
+		1 2 3
+		4 5 6
+		7 8 9
+	*/
+
+	public Layer(Corner cubie1Corner, Edge cubie2Edge, Corner cubie3Corner, Edge cubie4Corner, Center cubie5Center, Edge cubie6Edge, Corner cubie7Corner, Edge cubie8Edge, Corner cubie9Corner) {
+		this.cubie1Corner = cubie1Corner;
+		this.cubie2Edge = cubie2Edge;
+		this.cubie3Corner = cubie3Corner;
+		this.cubie4Corner = cubie4Corner;
+		this.cubie5Center = cubie5Center;
+		this.cubie6Edge = cubie6Edge;
+		this.cubie7Corner = cubie7Corner;
+		this.cubie8Edge = cubie8Edge;
+		this.cubie9Corner = cubie9Corner;
+
+	}
+
+
+
+
+}
