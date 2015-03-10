481004b7e7317e47683970260721291c6ddc7dc4
Andreas Nilsson <anil9@kth.se>
2015-03-10 13:52:11 +0100

    First commit

---
+++ Cube.java
@@ -0,0 +1,23 @@
+
+
+public class Cube {
+
+	public Cube() {
+		// 8 corners, 12(?) edges, 6 centers
+
+
+
+	}
+
+
+	// implement operations on the cube
+
+
+	public static void main(String [] args) {
+		Cube cube = new Cube();
+	}
+
+
+
+}
+
