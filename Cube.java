0cead987b8b5a02367d03319206d8f2fd25c9638
aspang1 <anton.spang@gmail.com>
2015-03-10 15:06:22 +0100

    La till funktionsskal för alla förflyttningar

--- Cube.java
+++ Cube.java
@@ -9,6 +9,56 @@ public class Cube {
 
 	}
 
+	//Notations 
+	// Twisting front face 90 degrees clockwise
+	public void F(){
+
+	}
+	// Twisting front face 90 degrees anti-clockwise
+	public void Fi(){
+
+	}
+	// Twisting Back face 90 degrees clockwise
+	public void B(){
+
+	}
+	// Twisting back face 90 degrees anti-clockwise
+	public void Bi(){
+
+	}
+	// Twisting right face 90 degrees clockwise
+	public void R(){
+
+	}
+	// Twisting right face 90 degrees anti-clockwise
+	public void Ri(){
+
+	}
+	// Twisting left face 90 degrees clockwise
+	public void L(){
+
+	}
+	// Twisting left face 90 degrees anti-clockwise
+	public void Li(){
+
+	}
+	// Twisting upper face 90 degrees clockwise
+	public void U(){
+
+	}
+	// Twisting upper face 90 degrees anti-clockwise
+	public void Ui(){
+
+	}
+	// Twisting bottom face 90 degrees clockwise
+	public void D(){
+
+	}
+	// Twisting bottom face 90 degrees anti-clockwise
+	public void Di(){
+
+	}
+
 
 	// implement operations on the cube
 
