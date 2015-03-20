54d88a96d4cfcf6047a2bdbd17d51e8353a280d5
Andreas Nilsson <anil9@kth.se>
2015-03-20 20:50:00 +0100

    Skal för Fridrich

---
+++ Fridrich.java
@@ -0,0 +1,24 @@
+
+
+public class Fridrich {
+
+	public Fridrich() {
+
+	}
+
+	public long runAlg(Cube cube){
+		long startTime = System.currentTimeMillis();
+
+		// run alg here
+		
+
+
+		long stopTime = System.currentTimeMillis(); - startTime;
+		return stopTime;
+	}
+
+
+
+
+
+}
